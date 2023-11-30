bb5c2ccd33a8daaec89b7c8fa7e3f710e7c6f06d
Ariane-5 <144696213+Ariane-5@users.noreply.github.com>
2023-11-30 11:09:38 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -5,7 +5,7 @@ def main():
     st.header("HOME PAGE")
     st.title("Projet 2 -Recommandation de Film pour le cinéma de la Creuse")
     st.subheader("Wild Code School 2023")
-    image = Image.open("image d'affiche.png")
+    image = Image.open("notre affiche.png")
     st.image(image, caption='')
 
 if __name__ == '__main__':
