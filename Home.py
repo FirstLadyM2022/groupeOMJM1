7437200ff4f5f9ac2cf5e53424c3c6ac50729b4b
Ariane-5 <144696213+Ariane-5@users.noreply.github.com>
2023-11-30 11:07:41 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -5,7 +5,7 @@ def main():
     st.header("HOME PAGE")
     st.title("Projet 2 -Recommandation de Film pour le cinéma de la Creuse")
     st.subheader("Wild Code School 2023")
-    image = Image.open(r"C:\Users\mirei\Desktop\dossier streamlit\pages\image d'affiche.png")
+    image = Image.open("image d'affiche.png")
     st.image(image, caption='')
 
 if __name__ == '__main__':
