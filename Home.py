4627e128654afc71ed1b9ad73027d217a3852afd
Ariane-5 <144696213+Ariane-5@users.noreply.github.com>
2023-11-30 14:40:09 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,6 +1,18 @@
 import streamlit as st
 from PIL import Image
 
+st.set_page_config(layout="wide")
+
+title1, title2 = st.columns([0.7, 0.3])
+
+with title1 :
+  st.title('Projet 2 : Système de recommandation de films')
+with title2 :
+  "Olmira, Mireille, Maxime, Julie" 
+  st.image('/app/groupeomjm1/pages/logo_WCS.png')
+
+#####
+
 def main():
     st.header("HOME PAGE")
     st.title("Projet 2 -Recommandation de Film pour le cinéma de la Creuse")
