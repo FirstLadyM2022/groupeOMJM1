72d30403668421a43fa6f367e6de2defb7f42e6a
Ariane-5 <144696213+Ariane-5@users.noreply.github.com>
2023-11-30 15:17:23 +0100

    Update 3_Sommaire.py

--- pages/3_Sommaire.py
+++ pages/3_Sommaire.py
@@ -16,10 +16,10 @@ with title2 :
   image = Image.open("/app/groupeomjm1/pages/logo_WCS.png")
   st.image(image, caption='')
 
-st.title('Première partie : Analyse de données')
+st.subheader('Première partie : Analyse de données')
 
 st.write("Il nous a été demandé premièrement de nettoyer et analyser une base de données contenant les caractérisqtiques de nombreux films, et d'en extraire une selection à proposer à un cinéma Français en perte de vitesse.")
 
-st.title('Seconde partie : Machine Learning')
+st.subheader('Seconde partie : Machine Learning')
 
 st.write("La deuxième tâche qui nous a été confiée consiste en la création d'un modèle de Machine Learning permettant de recommander des films en fonction d'une choix de film par l'utilisateur.")
