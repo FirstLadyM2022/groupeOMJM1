af1889a0c6eba1f9e180f259834e9582dcb4772e
Ariane-5 <144696213+Ariane-5@users.noreply.github.com>
2023-11-30 10:55:41 +0100

    Update accueil.py

--- pages/accueil.py
+++ pages/accueil.py
@@ -13,7 +13,7 @@ with title1 :
   st.title('Projet 2 : Système de recommandation de films')
 with title2 :
   "Olmira, Mireille, Maxime, Julie" 
-image = Image.open(r"C:\Users\mirei\Desktop\dossier streamlit\pages\logo_WCS.png")
+image = Image.open("logo_WCS.png")
 st.image(image, caption='')
 
 st.title('Première partie : Analyse de données')
