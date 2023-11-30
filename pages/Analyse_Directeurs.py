1bec446130cbf8cc53a8d8bef0c97677cdac8531
Ariane-5 <144696213+Ariane-5@users.noreply.github.com>
2023-11-30 15:08:09 +0100

    Update Analyse_Directeurs.py

--- pages/Analyse_Directeurs.py
+++ pages/Analyse_Directeurs.py
@@ -14,7 +14,7 @@ title1, title2 = st.columns([0.7, 0.3])
 with title1 :
   st.title('Projet 2 : Système de recommandation de films')
 with title2 :
-  "Olmira, Mireille, Maxime, Julie" 
+  "Julie, Mireille, Olmira, Maxime" 
   st.image('/app/groupeomjm1/pages/logo_WCS.png')
 
 #st.header("Données")
